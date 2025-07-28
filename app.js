5ae9039ff4f0eb3c8dd7a4d44c0c04f42c889d1f
DebasishaBehera <debasisha.behera.dev@gmail.com>
2025-07-28 23:38:01 +0530

    Adding Review Model

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const methodOverride = require("method-override");
 const ejsmate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-// const { error } = require("console");
 const { listingSchema } = require("./schema.js");
+const Review = require("./models/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -111,6 +111,20 @@ app.delete(
     res.redirect("/listings");
   })
 );
+
+//Reviews
+//Post Route
+app.post("/listings/:id/reviews", async(req,res) => {
+  let listing = await Listing.findById(req.params.id);
+  let newReview = new Review(req.body.review);
+  listing.reviews.push(newReview);
+
+  await newReview.save();
+  await listing.save();
+
+  console.log("new review saved");
+  res.redirect(`/listings/${listing._id}`)
+});
 
 // app.get("/testListing", async (req, res) => {
 //   let sampleListing = new Listing({
