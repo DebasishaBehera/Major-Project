71bca35b4982d90766770f0ec818b0f7cbc4a22b
DebasishaBehera <debasisha.behera.dev@gmail.com>
2025-08-15 00:04:23 +0530

    Implement Cloud SetUp

--- routes/listing.js
+++ routes/listing.js
@@ -3,25 +3,39 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
-
 const listingController = require("../controllers/listings.js");
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
 
-//Index route
-router.get("/", wrapAsync(listingController.index));
+router
+  .route("/")
+  //Index route
+  .get(wrapAsync(listingController.index))
+  //Create route
+  .post(
+    isLoggedIn,
+    upload.single("listing[image]"),
+    validateListing,
+    wrapAsync(listingController.createListing)
+  );
 
 //New route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 
-//Show route
-router.get("/:id", wrapAsync(listingController.showListing));
-
-//Create route
-router.post(
-  "/",
-  isLoggedIn,
-  validateListing,
-  wrapAsync(listingController.createListing)
-);
+router
+  .route("/:id")
+  //Show route
+  .get(wrapAsync(listingController.showListing))
+  //Update route
+  .put(
+    isLoggedIn,
+    isOwner,
+    validateListing,
+    wrapAsync(listingController.updateListing)
+  )
+  //Delete route
+  .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
 
 //Edit route
 router.get(
@@ -29,23 +43,6 @@ router.get(
   isLoggedIn,
   isOwner,
   wrapAsync(listingController.renderEditForm)
-);
-
-//Update route
-router.put(
-  "/:id",
-  isLoggedIn,
-  isOwner,
-  validateListing,
-  wrapAsync(listingController.updateListing)
-);
-
-//Delete route
-router.delete(
-  "/:id",
-  isLoggedIn,
-  isOwner,
-  wrapAsync(listingController.destroyListing)
 );
 
 module.exports = router;
